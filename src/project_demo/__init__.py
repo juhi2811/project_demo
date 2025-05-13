09625a631e2e8a9df32a406695b1bf00bda2049d
Juhi <juchic2811@gmail.com>
2025-05-13 14:42:04 +0200

    Add changes for main

--- src/project_demo/__init__.py
+++ src/project_demo/__init__.py
@@ -1,1 +1,5 @@
-from .main import run
+from .data_prep.prep_data import prepare_write_data
+from .model.train_model import train_model
+from .main import run, parse_args
+
+__all__ = ['prepare_write_data', 'train_model', 'run', 'parse_args']
